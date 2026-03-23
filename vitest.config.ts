ff7d10f95527426cf888d85b5045879e5c53c14d
Khalil Ltaief <ltaief.khalil@gmail.com>
2026-03-23 23:33:57 +0100

    feat: add Vite plugin for React and update dependencies in package.json and package-lock.json; add API tests for bosses, weapons, and items

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,16 @@
+import path from "path";
+
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  plugins: [react()],
   test: {
-    globals: true,
-    environment: "jsdom",
+    environment: "node",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
   },
 });
