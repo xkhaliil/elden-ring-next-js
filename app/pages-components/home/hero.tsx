5fafd2bde38952455063abd21caa48c5645db5f6
Khalil Ltaief <ltaief.khalil@gmail.com>
2026-03-23 11:16:49 +0100

    Refactor code structure for improved readability and maintainability

--- app/pages-components/home/hero.tsx
+++ app/pages-components/home/hero.tsx
@@ -3,11 +3,24 @@ import { PixelLiquidBg } from "@/components/unlumen-ui/pixel-liquid-bg";
 export function Hero() {
   return (
     <div className="relative h-screen min-h-screen">
-      <PixelLiquidBg pixelSize={12}>
-        <div className="flex h-full items-center justify-center">
-          <h1>Hello world</h1>
+      <PixelLiquidBg pixelSize={12} />
+      <div className="absolute inset-0 flex flex-col items-center justify-center gap-3 px-4 text-start">
+        <div className="flex flex-col text-5xl text-white shadow-2xl">The</div>
+        <div className="flex flex-col text-5xl text-white shadow-2xl">
+          Elden
         </div>
-      </PixelLiquidBg>
+        <div className="flex flex-col text-5xl text-white shadow-2xl">Ring</div>
+        <div className="flex flex-col text-5xl text-white shadow-2xl">WiKi</div>
+      </div>
+      <div>
+        <Image
+          src={logo}
+          alt="Logo"
+          width={60}
+          height={60}
+          className="absolute top-4 left-4 z-10"
+        />
+      </div>
     </div>
   );
 }
