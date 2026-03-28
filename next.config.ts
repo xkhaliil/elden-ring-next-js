5c76eb0771a91dc61bf030d80e9cfb7a3b15c574
Khalil Ltaief <ltaief.khalil@gmail.com>
2026-03-28 22:25:57 +0100

    feat: add Next.js bundle analyzer and configure analyze script in package.json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
+import withBundleAnalyzer from "@next/bundle-analyzer";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // ...your existing config
 };
 
-export default nextConfig;
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})(nextConfig);
